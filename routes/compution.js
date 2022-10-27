0e06b1d5a4e4ac5289de0a1051f3b3ee66452b4e
Prathyusha-Maram <s548821@nwmissouri.edu>
2022-10-26 20:58:29 -0500

    bonus

---
+++ routes/compution.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var x;
+
+  var rand = Math.random();
+  console.log(req.query.x);
+  var x = req.query.x;
+
+  if(x == undefined){
+    x = random.toFixed(2);
+  }
+
+  let imul = Math.imul(x).toFixed(2);
+  let log = Math.log(x).toFixed(2);
+  let log10 = Math.log10(x).toFixed(2);
+
+
+  res.render('computation', { 
+                title: 'computation',
+                c1: 'applied to '+x+ 'is '+imul,
+                c2: 'applied to '+x+ 'is '+log,
+                c3: 'applied to '+x+ 'is '+log10
+            });
+});
+
+module.exports = router;
